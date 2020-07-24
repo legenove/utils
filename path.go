f533644e182289e976e42cb2687b9dca00d9ff25
liuguoyang <396410414@qq.com>
2020-07-24 16:15:48 +0800

    init

---
+++ path.go
@@ -0,0 +1,31 @@
+package utils
+
+import "os"
+
+// FileExists reports whether the named file or directory exists.
+func FileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func PathExists(path string) bool {
+	_, err := os.Stat(path) //os.Stat获取文件信息
+	if err != nil {
+		if os.IsExist(err) {
+			return true
+		}
+		return false
+	}
+	return true
+}
+
+func CreateDir(path string) error {
+	if !PathExists(path) {
+		return os.MkdirAll(path, os.ModePerm)
+	}
+	return nil
+}
